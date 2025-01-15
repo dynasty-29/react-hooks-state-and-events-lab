8fbc9b06597272997b59721aec3f89644ee2aed9
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-01-15 04:32:43 +0300

    more on events

--- src/__tests__/Item.test.js
+++ src/__tests__/Item.test.js
@@ -10,6 +10,9 @@ test("the <li> does not have a className when initialized", () => {
 
 test("the <li> has a className of 'in-cart' when the Add to Cart button is clicked", () => {
   const { container } = render(<Item name="Milk" category="Dairy" />);
-  fireEvent.click(screen.getByText(/ Cart/));
+  // fireEvent.click(screen.getByText(/ Cart/));
+  const button = screen.getByText("Add to cart"); 
+  fireEvent.click(button);                        
+  
   expect(container.querySelector(".in-cart")).toBeInTheDocument();
 });
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [isInCart, setIsInCart] = useState(false);
+  function handleAddToCartClick() {
+    setIsInCart(!isInCart);
+  }
+  const cartClass = isInCart ? "in-cart" : "";
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCartClick}>{isInCart ? "Remove from cart": "Add to cart"}</button>
     </li>
   );
 }
